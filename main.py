e82991d87ba7d48309f3295949a83fd1f767b21c
Nik1tosik <nikit4ss@yandex.ru>
2023-03-26 20:13:32 +0300

    drawing a screen and a couple of simple shapes

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-print("Hello world")
-print("Hello world")
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480)) #flags=pygame.NOFRAME
+pygame.display.set_caption("First pygame")
+icon = pygame.image.load("images/icon.png")
+pygame.display.set_icon(icon)
+
+square = pygame.Surface((64, 64))
+square.fill('White')
+
+running = True
+while running:
+
+    pygame.draw.circle(screen, 'Red', (320, 240), 5)
+    screen.blit(square, (0, 0))
+
+    pygame.display.update()
+    screen.fill((146, 156, 148))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
